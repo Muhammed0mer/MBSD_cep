89a31108ebd1880aa28a3ed6567efd1dbe6abb5d
Muhammed0mer <m.umerbinmujeeb@gmail.com>
2022-07-28 08:15:50 +0500

    finaleyyyyyyyyyyyyyyyyyyyyyyyyy yayyyyyyyyyyyy

--- Locker_recieve.c
+++ Locker_recieve.c
@@ -1,8 +1,8 @@
 /*
- * recieveromer.c
+ * Receiver.c
  *
- * Created: 26/07/2022 10:32:20 pm
- * Author : Muhammed Omer
+ * Created: 7/26/2022 1:58:23 PM
+ * Author : TLS
  */ 
 
 #include <avr/io.h>
@@ -10,89 +10,98 @@
 #include<util/delay.h>
 #include<string.h>
 
-void USART1_initRecieve(void);
+void USART1_config(void);
 char USART1_recieveChar(void);
-char * USART1_recievePIN(void);
-void USART1_initTransmit(void);
-void USART1_sendChar(char data);
+char * USART1_recievePass();
+void USART1_sendStatus(char data);
 
 int main(void){
-	char latch_status = '1';
-	DDRB=0x08;
-	USART1_initTransmit();
-	USART1_initRecieve();
-	char PIN[4]={'0', '0' , '0','\0'};
+	
+	DDRC = 0xff; // for debugging
+	
+	char latch_status = 2;		// latch is closed initially
+	
+	// USART1
+	DDRB |= 1<<PB3;		// PB3 as op for Tx
+	DDRB &= ~(1<<PB2);	// PB2 as ip for Rx	
+	PORTB |= (1<<PB2);	// For error checking
+	USART1_config();
+	
+	char PIN[5]={3, 3 , 3, 3,'\0'};
 	while(1){
-		char PINattempt[4]={};
-        USART1_sendChar(latch_status);
-		if (latch_status=='0'){
-			strcpy(PINattempt,USART1_recievePIN());
-			if(PINattempt==PIN){
-				latch_status='1';
+		char PINattempt[5]={};
+			_delay_ms(100);
+		//PORTC=latch_status;
+		USART1_sendStatus(latch_status);
+		//_delay_ms(1000);
+		
+		if (latch_status==2){
+			//PORTC=0xff;
+					// latch is closed so compare the pass
+			strcpy(PINattempt,USART1_recievePass());	// PINattempt = USART1_recievePass() = Entered pass
+			//PORTC=0b11111111;
+			int i;
+			//for(i=0;i<5;i++){
+				//PORTC=PINattempt[i];
+				//_delay_ms(5000);
+			//}
+			
+			if(!(strcmp(PINattempt,PIN))){ // pass matched
+				latch_status=1; // opening the latch
 			}
+ 		
+ 			}
 			else{
-				if(USART1_recieveChar()=='1'){
-					latch_status='0';
-				}
+				
+ 				while(USART1_recieveChar()==1){} // push button not pressed
+					 PORTC=0b10101010;
+ 					latch_status=2;
+ 				
 			}
+		}
+	}
+
+void USART1_config(void)          // to configure USART1
+{
+	UBRR1H=0x00;
+	UBRR1L=0x05;
+	UCSR1B|=(1<<RXEN1)|(1<<TXEN1);
+	UCSR1C|=(1<<USBS1)|(3<<UCSZ10);
+}
+
+
+char * USART1_recievePass(void){
+	USART1_sendStatus(2);
+	char recieved_data;
+	static char PIN[5];
+	PIN[4]='\0';
+	int PINlength = 0;
+	while(PINlength<4){
+			recieved_data=USART1_recieveChar();
+			PORTC=recieved_data;
+			PIN[PINlength]=recieved_data;
+			PINlength++;
+			USART1_sendStatus(2);
 			
 		}
 		
-	}
-
+	return PIN;
 }
 
-void USART1_initRecieve(void){
-	DDRA=0xff;
-	DDRC=0xff;
-	UBRR1H=0x00;
-	UBRR1L=0x05;
-	UCSR1B=(1<<RXEN1);
-	UCSR1C=(3<<UCSZ10)|(1<<USBS1);
-}
-char USART1_recieveChar(void){
+char USART1_recieveChar(void){          // function to receive a character of pass
 	unsigned char recieved_data;
-	while(!(UCSR1A&(1<<UDRE1)));
+	
+	while(!(UCSR1A&(1<<RXC1)));
 	recieved_data=UDR1;
-	_delay_ms(100);
+	_delay_ms(50);
+	
 	return recieved_data;
 }
 
-char * USART1_recievePIN(void){
-	unsigned char recieved_data = USART1_recieveChar();
-	static char PIN[4];
-    PIN[4]='\0';
-	int PINlength = 0;
-	while(PINlength<4){
-		if(recieved_data=='#'){
-			PINlength=0;
-		}
-		else{
-			PIN[PINlength]=recieved_data;
-			PINlength++;
-			recieved_data=USART1_recieveChar();
-		}
-		
-	}
-    return PIN;
+void USART1_sendStatus(char data){
+	while(!(UCSR1A&(1<<UDRE1)));
+	UDR1=data;
+	char x = data;
+	//PORTC = x;
+	_delay_ms(50);
 }
-
-void USART1_initTransmit(void)
-{
-
-	UBRR1H=0x00;
-	UBRR1L=0x05;
-	UCSR1B=(1<<TXEN1);
-	UCSR1C=(3<<UCSZ10)|(1<<USBS1);
-}
-void USART1_sendChar(char data)
-{
-	char y = 0;
-	y=PINA & 0x01;
-	if(y==0){
-		while(!(UCSR1A&(1<<UDRE1)));
-		UDR1=data;
-		_delay_ms(100);
-	}
-}
-
--- Locker_sender.c
+++ Locker_sender.c
@@ -1,98 +1,126 @@
+/*
+ * Transmitter.c
+ *
+ * Created: 7/26/2022 1:56:22 PM
+ * Author : TLS
+ */ 
+
 #include <avr/io.h>
 #define F_CPU 1000000UL
 #include<util/delay.h>
 
 
-
+void USART1_config(void);
+void pushButton(void);
 int getKey(void);
-void USART1_initTransmit(void);
 void USART1_sendChar(char data);
-void USART1_initRecieve(void);
 char USART1_recieveChar(void);
 
 
 int main(void){
-    char digit[20]={'1','2','3','A','5','6','B','7','8','9','C','0','*','0','#','D','\0'};
-    int key;
-    DDRD=0x00;
-    DDRA = 0xFF;
-    DDRC=0x00;
-    DDRB=0x08;
-    DDRB &= ~(1<<PB0);
-    char latch_status = '1';
-    USART1_initTransmit();
-    USART1_initRecieve();
-    char PIN[4];
-    while(1){
-        latch_status=USART1_recieveChar()
+	
+	DDRE = 0xff; //For debugging	
+	DDRC=0b00000000; // For debugging
+	
+	// Keypad config
+	char digit[20]={1,2,3,'A',4,5,6,'B',7,8,9,'C','*',0,'#','D','\0'};
+	int key;
+	DDRD=0x00;			// Port D as i/p for keypad
+	DDRC &= ~(1<<PC0);	// PC0 as ip to read 
+	DDRC|=0b11111110;
+	DDRB |= (1<<PB1);  // PB1 as op to give DE = 0 to DE pin of keypad
+	
+	// 7 segment config
+	DDRA = 0xFF;		// Port A as op for 7 segment
+	
+	// USART1
+	DDRB |= 1<<PB3;		// PB3 as op for Tx
+	DDRB &= ~(1<<PB2);	// PB2 as ip for Rx	
+	PORTB |= (1<<PB2);	// For error checking
+	USART1_config();
+	
+	DDRB &= ~((1<<PB0));// PB0 as ip for push button 
+	
+	char latch_status;
+	
+	char btn;
+	
+//	char latch_status = '0'; // initilly latch is closed
+//	char PIN[4];
 
-        if (latch_status=='1'){
-            PORTA=0b11000000;
-        }
-        else{
-            PORTA=0b11000110;
-        }
-        if(latch_status=='0'){
-            key = getKey();
-            if (key!=16){
-        
-            USART0_sendChar(digit[key]);
+	while(1){
+//		PORTA=0b11000000; debugging
+//		PORTE |= (1<<PE1); 
+		latch_status=USART1_recieveChar();
+		//PORTC=0x00;
 
-            
-
-        }
-        }
-        else{
-        pushButton();
-        latch_status='0';
-        }
-        
-
-        
-        
-        
-       
-
-        }
-
-
-    }
-
-
-int getKey(void){
-    char x;
-    int data;
-    x=PINC;
-    if(x==0x01){
-        data=(PIND && 0x0f);
-        return data;
-    }
-    return 16;
+		if (latch_status==1){		// means latch is open
+			PORTA=0b11000000;
+			pushButton();
+			latch_status=2;
+			USART1_sendChar(latch_status);
+		}
+		if(latch_status==2){		// means latch is closed
+			PORTA=0b11000110;
+		
+			key = getKey();
+			//PORTC=0xff;
+			//_delay_ms(3000);
+					
+				PORTC=key;//key<<1
+				USART1_sendChar(digit[key]);
+			
+		}
+		
+			
+		
+	}
+	return 1;
 }
+
+// to get keypad key pressed position
+int getKey(void){
+	char y = 0;
+	int data;
+	char dat;
+	PORTB &= ~(0b00000010); // PB1 assigned 0 for DE
+	
+	y = PINC ;
+	PORTE=y;
+	//char y = x & 0b00000001;	// for reading DA (PC0) pin
+	//PORTE = y;
+			// if data available
+		data=(PIND & 0x0f);
+		 dat=data;
+		if((data>=0 && data<=2)| (data>=4 && data<=6) | (data>=8 && data <=10) | (data==13)){
+		
+		return data;
+	}
+	return 16;
+}
+
 // char() wait_for_pin(void){
 //     string pin;
 //     while(1){
-//         pin 
+//         pin
 //     }
 // }
-void USART1_initTransmit(void)
-{
 
-UBRR1H=0x00;
-UBRR1L=0x05;
-UCSR1B=(1<<TXEN1);
-UCSR1C=(3<<UCSZ10)|(1<<USBS1);
-}
-void USART1_sendChar(char data)
+void USART1_config(void)          // to configure USART1
 {
-char y = 0;
-    y=PINA & 0x01;
-    if(y==0){
-        while(!(UCSR1A&(1<<UDRE1)));
-        UDR1=data;
-        _delay_ms(100);
-    }
+	UBRR1H=0x00;
+	UBRR1L=0x05;
+	UCSR1B|=(1<<TXEN1)|(1<<RXEN1);
+	UCSR1C|=(1<<USBS1)|(3<<UCSZ10);
 }
+
+void USART1_sendChar(char data){
+	while(!(UCSR1A&(1<<UDRE1)));
+	
+	UDR1=data;
+	_delay_ms(100);
+}
+
 // void USART1_sendString(char *data)
 // {
 //     while(*data!='\0')
@@ -102,27 +130,26 @@ char y = 0;
 //     }
 // }
 
-void USART1_initRecieve(void){
-
-UBRR1H=0x00;
-UBRR1L=0x05;
-UCSR1B=(1<<RXEN1);
-UCSR1C=(3<<UCSZ10)|(1<<USBS1);
+char USART1_recieveChar(void){          // function to receive latch status after pass match from Receiver
+	char recieved_data;
+	
+	while(!(UCSR1A&(1<<RXC1)));
+	recieved_data=UDR1;
+	
+//	PORTC = recieved_data; // For debugging
+//	PORTE = recieved_data; // For debugging
+	_delay_ms(100);
+	
+	return recieved_data;
 }
-char USART1_recieveChar(void){
-    unsigned char recieved_data;
-    while(!(UCSR1A&(1<<UDRE1)));
-    recieved_data=UDR1;    
-    _delay_ms(100);
-    return recieved_data;
-    }
 
 void pushButton(){
-    while(1){
-	char x = PINB;
-	char y = x & 0b00000001;
-    if(y==1){
-        break;
-    }
-}
+	while(1){
+		char x = PINB;
+		char y = x & 0b00000001; // reading only PBO
+		if(y==0){				 // y = 0 means push button is pressed
+			break;
+		}
+		
+	}
 }
